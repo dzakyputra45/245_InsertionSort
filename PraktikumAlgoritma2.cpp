5f65811a14d6fe9e1dfd4ee34f0001becae3387f
dzakyputra45 <dzakypratama45@gmail.com>
2024-03-14 15:06:15 +0700

    Add step 4

--- PraktikumAlgoritma2.cpp
+++ PraktikumAlgoritma2.cpp
@@ -9,6 +9,14 @@ void insertiosort() {
         temp = arr[i]; //step 2
 
         j = i - 1; //step 3
+
+        while (j >= 0 && arr[j] > temp) //step 4
+        {
+            arr[j + 1] = arr[j]; //step 4a
+            j--; //step 4b
+        }
+
+
     }
 }
 
