3b4f6d3fbaed1857529fdec7012598322c5fda02
dzakyputra45 <dzakypratama45@gmail.com>
2024-03-14 15:07:21 +0700

    Add step 5

--- PraktikumAlgoritma2.cpp
+++ PraktikumAlgoritma2.cpp
@@ -7,7 +7,6 @@ void insertiosort() {
 
     for (i = 1; i <= n - 1; i++) { //step 1
         temp = arr[i]; //step 2
-
         j = i - 1; //step 3
 
         while (j >= 0 && arr[j] > temp) //step 4
@@ -16,7 +15,7 @@ void insertiosort() {
             j--; //step 4b
         }
 
-
+        arr[j + 1] = temp; //step 5
     }
 }
 
